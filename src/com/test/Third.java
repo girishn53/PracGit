187a26733adbc2630536d56989212296ac553d63
Girish <Girishn53@gmail.com>
2021-07-07 22:46:34 +0530

    Checkout from newbranch

--- src/com/test/Third.java
+++ src/com/test/Third.java
@@ -3,7 +3,7 @@ package com.test;
 public class Third {
 
 	public static void main(String[] args) {
-	System.out.println("From 3rd in master");
+	System.out.println("From 3rd in newbranch");
 
 	}
 
