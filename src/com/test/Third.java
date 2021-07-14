1fe5fc44900a02435b3f07931cf341ee45cbc05a
Girish <Girishn53@gmail.com>
2021-07-14 23:21:54 +0530

    newst

--- src/com/test/Third.java
+++ src/com/test/Third.java
@@ -5,8 +5,6 @@ public class Third {
 	public static void main(String[] args) {
 	System.out.println("From 3rd in newbranch");
 	System.out.println("New code");
-	System.out.println("This will be reset");
-	
 	
 	}
 
